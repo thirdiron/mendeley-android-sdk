12515467411afdf79f20bdd65d0b94c539c60bf2
David Ingram <david.ingram@mendeley.com>
2014-06-11 12:20:25 +0100

    Refactor TestRunner and add verbose option for test failures.

--- src/com/mendeley/api/network/tests/TestRunner.java
+++ src/com/mendeley/api/network/tests/TestRunner.java
@@ -2,28 +2,51 @@ package com.mendeley.api.network.tests;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
+/**
+ * Run the tests.
+ * <p>
+ * Usage: java -cp bin/classes.jar:libs/* com.mendeley.api.network.tests.TestRunner [-v]
+ */
 public class TestRunner {
-	public static void main(String[] args) {
-		Result result = JUnitCore.runClasses(NetworkConnectionTest.class);		
-//		for (Failure failure : result.getFailures()) {
-//			System.out.println("Tests error: "+failure.toString() + "\n"+failure.getTrace());
-//		}
-		System.out.println("Network connection passed: "+result.wasSuccessful());
-		
-		result = JUnitCore.runClasses(JsonParserTest.class);		
-		System.out.println("JsonParser passed: "+result.wasSuccessful());
+	private static String VERBOSE_FLAG = "-v";
 	
-		result = JUnitCore.runClasses(NetworkProviderTest.class);		
-		System.out.println("NetworkProvider passed: "+result.wasSuccessful());
-		
-		result = JUnitCore.runClasses(DocumentNetworkProviderTest.class);		
-		System.out.println("DocumentNetworkProvider passed: "+result.wasSuccessful());
-		
-		result = JUnitCore.runClasses(FileNetworkProviderTest.class);		
-		System.out.println("FileNetworkProvider passed: "+result.wasSuccessful());
-		
-		result = JUnitCore.runClasses(FolderNetworkProviderTest.class);		
-		System.out.println("FolderNetworkProvider passed: "+result.wasSuccessful());
+	private boolean verbose;
+	
+	public static void main(String[] args) {
+		// Parse args
+		boolean verbose = false;
+		for (String arg : args) {
+			if (arg.equals(VERBOSE_FLAG)) {
+				verbose = true;
+			}
+		}
+		// Run tests
+		new TestRunner(verbose);
+	}
+	
+	public TestRunner(boolean verbose) {
+		this.verbose = verbose;
+		doTests();
+	}
+	
+	private void doTests() {
+		runTest(NetworkConnectionTest.class);
+		runTest(JsonParserTest.class);
+		runTest(NetworkProviderTest.class);
+		runTest(DocumentNetworkProviderTest.class);
+		runTest(FileNetworkProviderTest.class);
+		runTest(FolderNetworkProviderTest.class);		
+	}
+	
+	private void runTest(Class<?> testClass) {
+		Result result = JUnitCore.runClasses(testClass);
+		if (verbose) {
+			for (Failure failure : result.getFailures()) {
+				System.out.println("Tests error: " + failure.toString() + "\n" + failure.getTrace());
+			}
+		}
+		System.out.println(testClass.getSimpleName() + (result.wasSuccessful() ? " passed" : " FAILED"));
 	}
 }  
