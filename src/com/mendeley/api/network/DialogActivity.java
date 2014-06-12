436f3b06eefae2997a929958bbde12422c978b42
Elad Schwarz <elad.schwarz@mendeley.com>
2014-06-12 17:07:05 +0100

    Changed signIn screen to use a webview in an activity with a dialog layout

---
+++ src/com/mendeley/api/network/DialogActivity.java
@@ -0,0 +1,245 @@
+package com.mendeley.api.network;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import com.mendeley.api.R;
+
+/**
+ * This activity will show the login web interface in a webview.
+ * The layout style will depend on the screen size. Full screen for small devices
+ * and a dialog view for larg ones.
+ */
+public class DialogActivity extends Activity {
+
+	final static double SMALL_SCREEN_SIZE = 7.0;
+	final static String FORGOT_PASSWORD_URL = "http://www.mendeley.com/forgot/";
+
+	WebView webView;
+	String authorizationCode;	
+	CredentialsManager credentialsManager;
+	JsonParser jsonParser = new JsonParser();	
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (getScreenSize() > SMALL_SCREEN_SIZE) {
+        	super.setTheme( android.R.style.Theme_Holo_Dialog);
+        }
+        
+        setContentView(R.layout.dialog_layout);
+
+        credentialsManager = new CredentialsManager(this);
+        
+        webView = (WebView) findViewById(R.id.dialogWebView);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.setVerticalScrollBarEnabled(true);
+		webView.setHorizontalScrollBarEnabled(true);
+	    webView.setVerticalScrollBarEnabled(true);
+	    webView.setHorizontalScrollBarEnabled(true);
+	    webView.requestFocusFromTouch();
+	    webView.getSettings().setUseWideViewPort(true);                                                         
+	    webView.getSettings().setLoadWithOverviewMode(true);
+	    webView.getSettings().setBuiltInZoomControls(true);
+		webView.setWebViewClient(new MendeleyWebViewClient());
+		webView.loadUrl(getOauth2URL());
+    }
+
+    /**
+     * Finding the screen size in inches
+     * 
+     * @return the screen size
+     */
+    private double getScreenSize() {
+    	DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        double x = Math.pow(dm.widthPixels/dm.xdpi,2);
+        double y = Math.pow(dm.heightPixels/dm.ydpi,2);
+        double screenInches = Math.sqrt(x+y);
+        screenInches =  (double)Math.round(screenInches * 10) / 10;
+
+        return screenInches;
+    }
+    
+	/**
+	 * Creating and return the Oauth2 url string.
+	 * 
+	 * @return the url string
+	 */
+	private String getOauth2URL() {
+		
+		StringBuffer urlString = new StringBuffer(AuthenticationManager.OUATH2_URL);
+		
+		urlString
+		.append("?").append("grant_type=").append(AuthenticationManager.GRANT_TYPE_AUTH)
+		.append("&").append("redirect_uri=").append(AuthenticationManager.REDIRECT_URI)
+		.append("&").append("scope=").append(AuthenticationManager.SCOPE)
+		.append("&").append("response_type=").append(AuthenticationManager.RESPONSE_TYPE)
+		.append("&").append("client_id=").append(credentialsManager.getClientID());
+		
+		return urlString.toString();
+	}
+	
+	/**
+	 * A WebViewClient that starts the AuthenticationTask when a new url is loaded.
+	 *
+	 */
+    private class MendeleyWebViewClient extends WebViewClient {
+    	
+    	@Override
+    	public boolean shouldOverrideUrlLoading (WebView view, String url) {
+
+    		if (url.equals(FORGOT_PASSWORD_URL)) {
+    			openUrlInBrowser(url);
+    		} else {
+    			new AuthenticateTask().execute(url);
+    		}
+			return true;    		
+    	}
+    }
+    
+    /**
+     * Opening a web browser to load the given url
+     * @param url the url to load
+     */
+    private void openUrlInBrowser(String url) {
+    	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url)); 
+    	startActivity(intent);
+    }
+    
+    /**
+	 * AsyncTask class that carry out the authentication task and send the
+	 * authorisation code with the result data, which will be received by
+	 * the SignInActivity
+	 */
+    class AuthenticateTask extends AsyncTask<String, Void, String> {
+
+    	protected String getJSONTokenString(String authorizationCode) throws ClientProtocolException, IOException {
+    		HttpResponse response = doPost(AuthenticationManager.TOKENS_URL, AuthenticationManager.GRANT_TYPE_AUTH, authorizationCode);
+    		String data = jsonParser.getJsonString(response.getEntity().getContent());
+	           
+    		return data;
+		}
+    	
+    	protected String getAuthorizationCode(String authReturnUrl) {
+    		
+    		String AuthorizationCode = null;
+			int index = authReturnUrl.indexOf("code=");	       			
+	        if (index != -1) {
+	        	index += 5;
+	        	AuthorizationCode = authReturnUrl.substring(index);
+	        }
+			
+			return AuthorizationCode;
+    	}
+    	
+		@Override
+		protected String doInBackground(String... params) {
+			
+			String result = null;
+			
+			authorizationCode = getAuthorizationCode(params[0]);			
+			if (authorizationCode != null) {
+				try {
+					String jsonTokenString = getJSONTokenString(authorizationCode);
+					getTokenDetails(jsonTokenString);
+					result = "ok";
+					
+				} catch (IOException e) {
+					Log.e("", "", e);
+
+				} catch (JSONException e) {
+					Log.e("", "", e);
+				}
+			}
+			
+			return result;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			
+			Intent resultData = new Intent(); 
+			if (result != null) {
+	        	resultData.putExtra("authorization_code", authorizationCode);
+			}
+			
+			setResult(Activity.RESULT_OK, resultData);
+			finish();
+		}    
+    }
+    
+	/**
+	 * Helper method for executing http post request
+	 * 
+	 * @param url the url string
+	 * @param grantType the grant type string
+	 * @param authorizationCode the authorisation code string
+	 * @return the HttpResponse object
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	HttpResponse doPost(String url, String grantType, String authorizationCode) throws ClientProtocolException, IOException {
+		
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(url);
+        
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(5);
+        nameValuePairs.add(new BasicNameValuePair("grant_type", grantType));
+        nameValuePairs.add(new BasicNameValuePair("redirect_uri", "http://localhost/auth_return"));
+        nameValuePairs.add(new BasicNameValuePair("code", authorizationCode));
+        nameValuePairs.add(new BasicNameValuePair("client_id", credentialsManager.getClientID()));
+        nameValuePairs.add(new BasicNameValuePair("client_secret", credentialsManager.getClientSecret()));
+        
+        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+        HttpResponse response = httpclient.execute(httppost);
+		  
+		return response;  
+	}
+	
+    /**
+     * Extracting the token details from the token string and sending them to the CredentialManager.
+     * 
+     * @param tokenString
+     * @throws JSONException
+     */
+	private void getTokenDetails(String tokenString) throws JSONException {
+		
+		JSONObject tokenObject = new JSONObject(tokenString);
+
+		String accessToken = tokenObject.getString("access_token");
+		String refreshToken = tokenObject.getString("refresh_token");	
+		String tokenType = tokenObject.getString("token_type");
+		int expiresIn = tokenObject.getInt("expires_in");
+
+		credentialsManager.setTokens(accessToken, refreshToken, tokenType, expiresIn);	
+	}
+}
