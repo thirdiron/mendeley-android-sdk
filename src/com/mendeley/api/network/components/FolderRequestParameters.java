809616d3d61651132e58d9470e4eb8ec14e53dff
M <elad.schwarz@mendeley.com>
2014-07-14 11:28:15 +0100

    added limit to FolderRequestParameters

--- src/com/mendeley/api/network/components/FolderRequestParameters.java
+++ src/com/mendeley/api/network/components/FolderRequestParameters.java
@@ -10,5 +10,11 @@ public class FolderRequestParameters extends MendeleyRequest {
 	 * Group ID. If not supplied, returns user folders.
 	 */
 	public String groupId;
+	
+	/**
+	 * The maximum number of items on the page. If not supplied, the default is 20. The largest allowable value is 500.
+	 */
+	public Integer limit;
+	
 
 }
