518e14f6f8beab7c22f405f91e4a1125b68c3306
M <elad.schwarz@mendeley.com>
2014-07-23 11:21:35 +0100

    updated fetFiles callback

--- src/com/mendeley/api/callbacks/file/GetFilesCallback.java
+++ src/com/mendeley/api/callbacks/file/GetFilesCallback.java
@@ -4,9 +4,10 @@ import com.mendeley.api.exceptions.MendeleyException;
 import com.mendeley.api.model.File;
 import com.mendeley.api.params.Page;
 
+import java.util.Date;
 import java.util.List;
 
 public interface GetFilesCallback {
-    public void onFilesReceived(List<File> files, Page next);
+    public void onFilesReceived(List<File> files, Page next, Date serverDate);
     public void onFilesNotReceived(MendeleyException mendeleyException);
 }
