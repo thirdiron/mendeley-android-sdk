78c6f6713a765d4d96f216abaac03d0d3521ad58
David Ingram <david.ingram@mendeley.com>
2014-10-07 16:30:09 +0100

    AND-228: No longer require the document ID when downloading a file.

--- library/src/com/mendeley/api/callbacks/file/GetFileCallback.java
+++ library/src/com/mendeley/api/callbacks/file/GetFileCallback.java
@@ -3,7 +3,7 @@ package com.mendeley.api.callbacks.file;
 import com.mendeley.api.exceptions.MendeleyException;
 
 public interface GetFileCallback {
-    public void onFileDownloadProgress(String fileId, String documentId, int progress);
-    public void onFileReceived(String fileId, String documentId, String fileName);
-    public void onFileNotReceived(String fileId, String documentId, MendeleyException mendeleyException);
+    public void onFileDownloadProgress(String fileId, int progress);
+    public void onFileReceived(String fileId, String fileName);
+    public void onFileNotReceived(String fileId, MendeleyException mendeleyException);
 }
